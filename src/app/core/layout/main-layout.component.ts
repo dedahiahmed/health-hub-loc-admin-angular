2f57ab31a4d58260319dac893b1e5284da67b6ad
dedahiahmed <dedahiahmed10@gmail.com>
2025-03-09 08:52:41 +0000

    add logout button

--- src/app/core/layout/main-layout.component.ts
+++ src/app/core/layout/main-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service'; // Assuming you're using ngx-cookie-service
 
 @Component({
   selector: 'app-main-layout',
@@ -11,7 +12,14 @@ import { RouterModule } from '@angular/router';
 export class MainLayoutComponent {
   isSidebarOpen = false;
 
+  constructor(private cookieService: CookieService, private router: Router) {}
+
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  logout() {
+    this.cookieService.delete('token');
+    this.router.navigate(['/login']);
   }
 }
